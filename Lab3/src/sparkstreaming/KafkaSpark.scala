05890af76f629bb07bba06f856ec8a96e98dc1d0
ferles <you@example.com>
2018-10-06 00:40:33 +0200

    Code is done, also added a results folder and added a screenshot of a temprary result

--- Lab3/src/sparkstreaming/KafkaSpark.scala
+++ Lab3/src/sparkstreaming/KafkaSpark.scala
@@ -17,29 +17,65 @@ import org.apache.spark.sql.cassandra._
 import com.datastax.spark.connector._
 import com.datastax.driver.core.{Session, Cluster, Host, Metadata}
 import com.datastax.spark.connector.streaming._
+import org.apache.spark.sql.cassandra._
+import com.datastax.spark.connector._
+import com.datastax.driver.core.{Session, Cluster, Host, Metadata}
 
 object KafkaSpark {
   def main(args: Array[String]) {
     // connect to Cassandra and make a keyspace and table as explained in the document
-    val cluster = Cluster.<FILL IN>
+    val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
     val session = cluster.connect()
-    session.execute(.<FILL IN>)
+
+    session.execute("CREATE KEYSPACE IF NOT EXISTS avg_space WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };")
+    session.execute("CREATE TABLE IF NOT EXISTS avg_space.avg (word text PRIMARY KEY, count float);")
 
     // make a connection to Kafka and read (key, value) pairs from it
-    <FILL IN>
-    val kafkaConf = <FILL IN>
-    val messages = KafkaUtils.createDirectStream.<FILL IN>
-    <FILL IN>
+
+    // Credits to https://jaceklaskowski.gitbooks.io/spark-streaming/content/spark-streaming-kafka.html#no-receivers
+    // for help
+    val conf = new SparkConf().setMaster("local[*]").setAppName("ID2221 Lab 3")
+    val ssc = new StreamingContext(conf, batchDuration = Seconds(5))
+
+//    val initialRDD = ssc.sparkContext.parallelize(List(("a", 0)))
+
+
+    val kafkaConf = Map(
+      "metadata.broker.list" -> "localhost:9092",
+      "zookeeper.connect" -> "localhost:2181",
+      "group.id" -> "kafka-spark-streaming",
+      "zookeeper.connection.timeout.ms" -> "1000")
+
+    val kafkaTopics= Set("avg")
+    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaConf, kafkaTopics)
+
+    def help_split(pair: String): (String, Double)={
+
+      val splited = pair.split(",")
+      (splited(0), splited(1).toInt)
+
+    }
+
+    val pairs = messages.map(_._2).map(help_split)
 
     // measure the average value for each key in a stateful manner
-    def mappingFunc(key: String, value: Option[Double], state: State[Double]): (String, Double) = {
-	<FILL IN>
+    def mappingFunc(key: String, value: Option[Double], state: State[(Double, Int)]): (String, Double) = {
+
+      val help_value = value.getOrElse(0D)
+      val help_state = state.getOption.getOrElse((0D, 0))
+
+      state.update(help_state._1 + help_value, help_state._2+ 1)
+      val avg = state.get()._1 / state.get()._2
+
+      (key, avg)
     }
-    val stateDstream = pairs.mapWithState(<FILL IN>)
+
+    val stateDstream = pairs.mapWithState(StateSpec.function(mappingFunc _))
 
     // store the result in Cassandra
-    stateDstream.<FILL IN>
+    stateDstream.saveToCassandra("avg_space" ,"avg")
 
+    ssc.checkpoint("./checkpoints")
     ssc.start()
     ssc.awaitTermination()
   }
